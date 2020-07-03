dcb27b06e5ba7facda90a67fe8749344d4db82f4
Neal Callaghan <neal.callaghan@blueprism.com>
2020-07-03 20:46:31 +0100

    @master - added new controller

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,15 @@
+﻿namespace StarChart.Controllers
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context) =>
+            _context = context;
+    }
+}
