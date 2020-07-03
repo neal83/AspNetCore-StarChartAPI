8e32bcd72e115b95462a63b25297050221257049
Neal Callaghan <neal.callaghan@blueprism.com>
2020-07-03 20:30:30 +0100

    @master - added properties and annotations to the celestial object model, added a db set to the context

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿namespace StarChart.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using Models;
 
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
